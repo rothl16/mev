9d87a8f884d233b4ab70d39674f7d1bc5c748782
rothl16 <leopold.roth@univie.ac.at>
2024-03-09 19:36:14 +0100

    checked off some ToDos

--- functions_run_first.R
+++ functions_run_first.R
@@ -5,7 +5,8 @@ clean_data <- function(df, progress_threshold = 100, AC1_value = 7, AC2_value = 
     filter(AC1 == AC1_value) %>% #Exclude if attention check 1 was missed
     filter(AC2 == AC2_value) %>% #Exclude if attention check 2 was missed
     filter(Duration__in_seconds > (mean(Duration__in_seconds, na.rm = TRUE) - 3 * sd(Duration__in_seconds, na.rm = TRUE))) %>%#Exclude if time is 3SD below mean
-    filter(Lan < 3)
+    filter(Lan < 3) %>% #language < very good
+    filter(Cou < 2) #residency != target country
   
   return(filtered_df)
 }
